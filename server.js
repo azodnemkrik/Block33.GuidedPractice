5979d3945e2bd44c5faec7460ed0506b64b3bc6b
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-03 00:22:38 -0500

    Added SQL Tables

--- server.js
+++ server.js
@@ -6,8 +6,30 @@ const client = new pg.Client("postgres://localhost/acme_notes_categories_db" || 
 const init = async (req,res,next) => {
     // Make a connection
     await client.connect()
-    
+
     // Create the tables
+    let SQL = `
+
+        DROP TABLE IF EXISTS notes;
+        DROP TABLE IF EXISTS categories;
+
+        CREATE TABLE categories(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+        );
+
+        CREATE TABLE notes(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50),
+            txt VARCHAR(50),
+            created_at TIMESTAMP DEFAULT now(),
+            updated_at TIMESTAMP DEFAULT now(),
+            ranking DEFAULT 3 NOT NULL,
+            category_id DEFAULT 3 NOT NULL
+        );
+        
+
+    `
 
     // Create a port & listen
     const PORT = 3000
