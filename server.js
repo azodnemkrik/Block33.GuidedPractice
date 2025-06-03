dca4ca767b0da3289c9121e78ff221fb088dfc91
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-03 00:12:18 -0500

    connected PG

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express()
 const pg = require("pg")
 const client = new pg.Client("postgres://localhost/acme_notes_categories_db" || process.env.DATABASE)
 
-const init = () => {
-
+const init = async (req,res,next) => {
     // Make a connection
+    await client.connect()
+    
     // Create the tables
 
     // Create a port & listen
